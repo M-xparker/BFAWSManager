52ca5ad1c1a140bb09c245f14b4a20b87f901dea
Matt Parker <mdparker89@gmail.com>
2013-03-02 21:31:32 -0500

    relink files

--- BFAWSManager.h
+++ BFAWSManager.h
@@ -1,25 +1,9 @@
 //
-//  BFAWSManager.h
+//  BFAWSManagerClass.h
+//  ExampleAppGA
 //
-//  Created by Matthew Parker on 12/18/12.
-//  Copyright (c) 2012 BitFountain.
-//Permission is hereby granted, free of charge, to any person obtaining a copy
-//of this software and associated documentation files (the "Software"), to deal
-//in the Software without restriction, including without limitation the rights
-//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//copies of the Software, and to permit persons to whom the Software is
-//furnished to do so, subject to the following conditions:
-//
-//The above copyright notice and this permission notice shall be included in
-//all copies or substantial portions of the Software.
-//
-//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//THE SOFTWARE.
+//  Created by Matthew Parker on 3/2/13.
+//  Copyright (c) 2013 Matt Parker. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
@@ -34,5 +18,4 @@
 +(void)deleteFileWithName:(NSString *)filename;
 +(NSURLRequest *)getURLforFileWithName:(NSString *)filename;
 +(NSMutableArray *)s3DirectoryListingWithPrefix:(NSString *)prefix;
-
 @end
